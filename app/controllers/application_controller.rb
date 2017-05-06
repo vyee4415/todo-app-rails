c4c0d1184465ebe5ac0ea20891c41670c6203274
Vivian Yee <vyee4415@bths.edu>
2017-05-06 15:11:19 +0000

    stuff2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello 
-    render html: "Hello CodeNow!" 
-    render  html: "http://cdn2.business2community.com/wp-content/uploads/2011/03/1095867_56160494.jpg"
+  def hello
+    render html: "http://cdn2.business2community.com/wp-content/uploads/2011/03/1095867_56160494.jpg"
   end
  
 end
