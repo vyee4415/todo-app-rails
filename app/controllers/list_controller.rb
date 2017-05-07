153479839516e955b2b9f1ab2b17bb4d6d717103
Vivian Yee <vyee4415@bths.edu>
2017-05-07 19:12:49 +0000

    END

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -20,5 +20,28 @@ class ListController < ApplicationController
         { name: 'D:', is_checked: false } 
       ]
     end
+    l = List.find_by_id(@id)
+    @list_name = l.name
+  end
+  def new
+    l = List.new 
+    l.name = params['name'] 
+    l.save 
+    redirect_to "/list/#{ l.id }"
+  end
+  def create
+  
+  end
+  def edit
+    l = List.find_by_id(params['id'])
+    @list_name = l.name
+    @list_id = l.id
+  end
+  def update
+    l = List.find_by_id(params['id'])
+    l.name = params['name']
+    l.save
+    redirect_to "/list/#{l.id}" 
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   get 'list/index'
+  get "list/new"
+  get "list/create"
+  get "list/edit/:id" =>"list#edit"
+  get "list/update/:id" =>"list#update"
   get "list/:id" => "list#show"
   
   root 'application#hello'
